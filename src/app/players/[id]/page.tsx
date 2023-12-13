b3b6b47f8f568329663fe3e6c16677d64fe0d888
javier <elteliblejvi@gmail.com>
2023-12-13 18:15:42 +0100

    update

--- src/app/players/[id]/page.tsx
+++ src/app/players/[id]/page.tsx
@@ -12,16 +12,19 @@ const PlayersEdit = () => {
     await updatePlayer(Number(id) as number, data as FormData);
   };
   return (
-    <form className="pb-8 bg-red-600" action={update}>
+    <div className="flex flex-col items-center pt-10">
+          <form className="pb-8 flex flex-col  w-[20rem]" action={update}>
       <input type="text" name="name" aria-label="Name" />
-      <input type="text" name="position" aria-label="Position" />
+      <input type="text" name="position" aria-label="Position" className="mt-4" />
       <button
         type="submit"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white mt-4 font-bold py-2 px-4 rounded"
       >
         Update Player
       </button>
     </form>
+    </div>
+
   );
 };
 
